1afc98daf7f06c12c1c1a7e0b38707ee513dc943
WalzerMelodica <WalzerMelodica@tutanota.com>
2020-11-26 20:54:57 +0300

    1st

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #write your code below this line
+    firstans=input('Greetings! How are you doing?')
+    secondans=input('Oh, how interesting. Tell me more!')
+    print('Thanks for sharing!')
+
 
 if __name__ == '__main__':
     main()
